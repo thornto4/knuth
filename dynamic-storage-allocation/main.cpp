ddf55ee8ed33db6a1ddced2db260093d673ad20f
Ryan Thornton <thornto4@gmail.com>
2015-08-20 01:01:44 -0500

    Updating main to demonstrate new enhanced-logging capabilities nad also free our allocated block of memory.

--- dynamic-storage-allocation/main.cpp
+++ dynamic-storage-allocation/main.cpp
@@ -2,10 +2,17 @@
 
 #include "BuddyAllocator.h"
 
+#include <string.h>
+
 int main()
 {
     BuddyAllocator buddy(8u); // 2^8 byte buffer
-    buddy.alloc(4);
+    buddy.showDetails(true);
+
+    char *foo = buddy.alloc(11);
+    strcpy(foo, "HelloWorld");
+    buddy.print();
+    buddy.free(foo);
     buddy.print();
     return 0;
 }
